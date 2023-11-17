740b3dd3cbcfd977b5524a65dada04afb2d83710
GuilhermeRippel <guilhermerippelmoraes@gmail.com>
2023-11-16 23:03:38 -0300

    Projeto calculadora de IMC pronto

---
+++ Algoritmos e Logica de Programação/calculadoraDeIMC2/js/script.js
@@ -0,0 +1,38 @@
+import { modal } from './modal.js'
+
+/*variáveis*/
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+const alertError = document.querySelector('#error')
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const showAlertError = notANumber(weight) || notANumber(height)
+
+    if(showAlertError){
+        alertError.classList.add('open')
+        return;
+    } else {
+        alertError.classList.remove('open')
+    }
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+    modal.message.innerText = message
+
+    modal.open()
+}
+
+function notANumber(value){
+    return isNaN(value) || value == ""
+}
+
+function IMC(weight, height){
+    return ( weight / ((height / 100) ** 2)).toFixed(2)
+}
+
