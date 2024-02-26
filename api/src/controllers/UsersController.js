0b627e1e2bae7cd75ce1b03aff9f09d55b35a48c
Guilherme <guilhermerippelmoraes@gmail.com>
2024-02-26 18:25:00 -0300

    Criptografando senha

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const {hash} = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
@@ -12,10 +13,18 @@ class UsersController {
         if(checkUserExist){
             throw new AppError("Este e-mail já está em uso")
         }
+
+        const hashedPassword = await hash(password, 8)
         
-        await database.run("INSERT INTO users (name, email, password)VALUES (?, ?, ?)", [ name, email, password ]);
+        await database.run("INSERT INTO users (name, email, password)VALUES (?, ?, ?)", [ name, email, hashedPassword ]);
 
         return res.status(201).json()
+    }
+
+    async update(req, res){
+        const {name, email} = req.body
+
+        const {id} = req.params
     }
 }
 // Um controller por boa prática pode ter no máximo 5 funções/métodos
