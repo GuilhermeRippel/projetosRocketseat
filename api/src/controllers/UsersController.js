2ea1f73694af6f1e00342e85dab48102ec6c547a
Guilherme <guilhermerippelmoraes@gmail.com>
2024-02-26 00:32:10 -0300

    Criando cadastro de usuário

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,14 +1,21 @@
 const AppError = require("../utils/AppError")
 
+const sqliteConnection = require("../database/sqlite")
+
 class UsersController {
-    create(req, res){
+    async create(req, res){
         const {name, email, password} = req.body
 
-        if(!name){
-            throw new AppError("Nome obrigatório")
-        }
+        const database = await sqliteConnection()
+        const checkUserExist = await database.get('SELECT *FROM users WHERE email = (?)', [email])
 
-        res.status(201).json({name, email, password}) 
+        if(checkUserExist){
+            throw new AppError("Este e-mail já está em uso")
+        }
+        
+        await database.run("INSERT INTO users (name, email, password)VALUES (?, ?, ?)", [ name, email, password ]);
+
+        return res.status(201).json()
     }
 }
 // Um controller por boa prática pode ter no máximo 5 funções/métodos
