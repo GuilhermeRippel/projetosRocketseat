1683ce96b57886c017fbfffaff8f9284a3df6f07
Guilherme <guilhermerippelmoraes@gmail.com>
2024-03-18 17:09:47 -0300

    Trabalhando com componentes e estilizações React

--- ProjetosReact/rocketnotes/src/pages/Details/index.jsx
+++ ProjetosReact/rocketnotes/src/pages/Details/index.jsx
@@ -1,12 +1,15 @@
 import { Container } from './styles'
 
-export function Details(){
+import { Button } from '../../components/Button'
 
+export function Details(){
 
   return(
     <Container>
         <h1>Hello World!</h1>
         <span>Guilherme Rippel</span>
+
+        <Button/>
     </Container>
   )
 }
